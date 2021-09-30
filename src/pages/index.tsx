e2d019d26342353786b1c7ea4874555c74489671
kacperjagiela <jagielakacper80@gmail.com>
2021-09-30 16:04:31 +0200

    config: initial project configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => (
+    <>
+        <Head>
+            <title>StarWars Battle It Out</title>
+            <meta name="description" content="Developed by Kacper Jagieła" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+    </>
+);
+
+export default Home;
