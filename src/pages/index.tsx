839bcba3bf364e90275662c3e512cf7efc6f4576
kacperjagiela <jagielakacper80@gmail.com>
2021-09-30 16:07:16 +0200

    config: add material-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => (
             <title>StarWars Battle It Out</title>
             <meta name="description" content="Developed by Kacper Jagieła" />
             <link rel="icon" href="/favicon.ico" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
         </Head>
     </>
 );
